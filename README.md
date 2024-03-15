ac5ce5a94fb4715ec6e062de6d09b897cfcbd911
Jacob Su <suzp1984@gmail.com>
2024-03-15 14:24:33 +0800

    add tutorial 1,2,3 code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gstreamer-examples
+
+gstreamer-tutorial is source code practise of [offical tutorial](https://gstreamer.freedesktop.org/documentation/tutorials/basic/)
+
+# config MacOS env
+
+```
+. ./source_macox_env_gstreamer.sh
+```
---
+++ source_macox_env_gstreamer.sh
@@ -0,0 +1,5 @@
+# Tell pkg-config where to find the .pc files
+export PKG_CONFIG_PATH=/Library/Frameworks/GStreamer.framework/Versions/1.0/lib/pkgconfig
+
+# We will use the pkg-config provided by the GStreamer.framework
+export PATH=/Library/Frameworks/GStreamer.framework/Versions/1.0/bin:$PATH
